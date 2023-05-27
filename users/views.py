866464af4fe5e9f1635b45fcec26dc0867b36d40
Linus Heinsvig <heinsvig@icloud.com>
2023-05-27 17:53:57 +0000

    Add profile page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from . import forms
 
 # Create your views here.
@@ -17,3 +18,8 @@ def register(request):
     else:        
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
