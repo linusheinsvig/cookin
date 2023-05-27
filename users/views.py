c503f9194c05bc017c7e42030d7401697dff5712
Linus Heinsvig <heinsvig@icloud.com>
2023-05-27 17:34:04 +0000

    Add log-in and log-out functions

--- users/views.py
+++ users/views.py
@@ -12,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"{username}, your account has been created")
-            return redirect('cookin-home')
+            messages.success(request, f"{username}, your account has been created, please login")
+            return redirect('user-login')
     else:        
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
